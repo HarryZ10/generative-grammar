278791500251a9022b0e0773b9576eec107082ef
HarryZ10 <hzhu20@georgefox.edu>
2023-09-17 12:22:49 -0700

    unicode

--- grammar.py
+++ grammar.py
@@ -13,9 +13,17 @@ class RandomTextGenerator:
         self._read_grammar_rules()                  # entrypoint to the program
 
     def _read_grammar_rules(self) -> None:
-        with open(self.grammar_file, 'r') as file:
+        try:
+            with open(self.grammar_file, 'r', encoding='UTF-8') as file:
+                self._process_file(file)
+        except UnicodeDecodeError:
+            with open(self.grammar_file, 'r', encoding='ISO-8859-1') as file:
+                self._process_file(file)
+
+    def _process_file(self, file) -> None:
             eof: bool = False
             while not eof:
+
                 line = file.readline()
                 if not line:
                     # stop processing productions
@@ -49,7 +57,16 @@ class RandomTextGenerator:
     def _consume_set_of_productions(self, file) -> list:
         productions: list = [] 
         while not (line := file.readline().strip()).startswith("}"):
-            productions.append(line[:-1]) if line.endswith(";") else productions.append(line)
+            if line.endswith(";"):
+                # remove the last character (semicolon) and split the line into individual productions
+                for prod in line[:-1].split(";"):
+                    # strip whitespace from the production
+                    prod = prod.strip()
+                    if prod:
+                        productions.append(prod)
+            else:
+                productions.append(line)
+
         return productions
 
     def _get_content(self, non_terminal) -> list:
